cbbd971069526cba504ba13010c82e94f59aff74
ampere07 <ravenampere0123@gmail.com>
2025-02-04 18:16:03 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,13 +20,24 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://reignco.vercel.app', 'http://localhost:5173'],
+const corsOptions = {
+  origin: function(origin, callback) {
+    const allowedOrigins = ['https://reignco.vercel.app', 'http://localhost:5173'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept']
-}));
+};
 
+app.use(cors(corsOptions));
+
+// Add preflight handling for all routes
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 const storage = new CloudinaryStorage({
