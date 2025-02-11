1224c09046dda4cf205f8382e4b626810f0ae25e
ampere07 <ravenampere0123@gmail.com>
2025-02-11 05:14:03 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,8 +37,9 @@ app.options('*', cors());
 app.use(express.json());
 
 // Add security headers middleware
-app.use((res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
